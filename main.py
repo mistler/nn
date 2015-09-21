7fa12719da2ff346adc1ffd0f6cdf455a988a282
mistler <mistler@mail.ru>
2015-09-21 23:21:31 +0600

    validation

--- main.py
+++ main.py
@@ -21,31 +21,29 @@ parser.add_option('-p', '--prediction', dest='prediction', default=3)
 
 
 if options.validation:
+    print 'Validating...'
     with open(options.network, 'rb') as f:
         nn = pickle.load(f)
-
-
-print 'Building network...'
-if options.resume:
-    print 'Resume training'
-    with open(options.network, 'rb') as f:
-        nn = pickle.load(f)
+    nn.validate()
 else:
-    print 'Loading data...'
-    data = Data(options.dataFileName)
-    print 'Data size: ', len(data)
-    networkModule = importlib.import_module('networks.' + options.network[:1].lower() + options.network[1:])
-    networkClass = getattr(networkModule, options.network)
-    nn = networkClass(data)
+    print 'Building network...'
+    if options.resume:
+        print 'Resume training'
+        with open(options.network, 'rb') as f:
+            nn = pickle.load(f)
+    else:
+        print 'Loading data...'
+        data = Data(options.dataFileName)
+        print 'Data size: ', len(data)
+        networkModule = importlib.import_module('networks.' + options.network[:1].lower() + options.network[1:])
+        networkClass = getattr(networkModule, options.network)
+        nn = networkClass(data)
 
-print 'Training...'
-nn.train()
+    print 'Training...'
+    nn.train()
 
-print "saving nn..."
-name = ''
-for s in sys.argv[1:]:
-    name += s
-name += '.pkl'
-with open(name, 'w') as f:
-    pickle.dump(nn, f)
+    print "saving nn..."
+    name = options.network + '.pkl'
+    with open(name, 'w') as f:
+        pickle.dump(nn, f)
 
